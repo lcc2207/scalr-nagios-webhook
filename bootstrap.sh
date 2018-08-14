001194bce41bdb3b8b9c2f145702b80798fd946c
lcc2207 <lynn@fidmail.com>
2018-08-14 11:28:00 -0500

    update config

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,4 +7,9 @@ useradd -g uwsgi uwsgi
 
 pip install -r /opt/nagios-webhook/requirements.txt
 
-/usr/bin/uwsgi --ini /opt/nagios-webhook/uwsgi.ini
+cp nagios-webhook.service /etc/systemd/system/
+
+systemctl start nagios-webhook
+systemctl enable nagios-webhook
+
+#/usr/bin/uwsgi --ini /opt/nagios-webhook/uwsgi.ini --logto2 /var/log/nagios-webhook.log
